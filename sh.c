349bc37f9d4f6490cdf3fde4e4f70386a0c880dc
ohamill <47223204+ohamill@users.noreply.github.com>
2020-11-23 20:54:49 -0500

    Support multiple pipes

--- sh.c
+++ sh.c
@@ -13,30 +13,26 @@
 enum {
 	INPUTSZ = 512,
 	BUFSZ = 512,
-	EXECSZ = 512
+	EXECSZ = 512,
+	ARGSZ = 50
 };
 
-char execPath[EXECSZ];
-char *args[50];
-int pid, wstatus, i, fd;
-
-char *formatcmd(char []);
-void tokenizecmd(char []);
+void addSpaces(char [], char[]);
+void tokenizecmd(char [], char*[]);
 void runcmd(char *[]);
-void outredir(char *[], char *, bool);
-void inredir(char *[], char *);
-void piperedir(char *[]);
-void ampcmd(char *[]);
+void piperedir(char *[], int);
+int pipetokenize(char *[], int, char *[]);
 
 int main(void) {
-	char usrinput[INPUTSZ], buf[BUFSZ];
-	char *bufptr, *pwname, *pwdir, *cmd;
+	char buf[BUFSZ], usrinput[INPUTSZ], newusrinput[INPUTSZ];
+	char *args[ARGSZ];
 	struct passwd *pw;
+	char *pwname, *bufptr;
+	int wstatus, pid, i, pipesymbol;
 
 	// Get user's home directory and username
 	pw = getpwuid(getuid());
 	pwname = pw->pw_name;
-	pwdir = pw->pw_dir;
 
 	while (true) {
 		// Check for completed background processes
@@ -52,200 +48,202 @@ int main(void) {
 			printf("\n");
 			break;
 		}
-		cmd = formatcmd(usrinput);
-		tokenizecmd(cmd);
-		if (i == 0) {
-			continue;
+
+		addSpaces(usrinput, newusrinput);
+		tokenizecmd(newusrinput, args);
+
+		// Check for pipes
+		i = 0;
+		pipesymbol = 0;
+		while (args[i] != NULL) {
+			if (*args[i] == '|') {
+				pipesymbol++;
+			}
+			i++;
 		}
-		args[i] = NULL;
-		runcmd(args);
-		free(cmd);
+		if (pipesymbol) {
+			piperedir(args, pipesymbol);
+		} else {
+			runcmd(args);
+		}
 	}
 }
 
-char *formatcmd(char cmd[]) {
-	char *new = malloc(sizeof(char) * (strlen(cmd) + 1));
+void addSpaces(char old[], char new[]) {
+	int j;
 
-	for (int i = 0, j = 0; i <= strlen(cmd); i++) {
-		if (cmd[i] == '&' || cmd[i] == '<' || cmd[i] == '|') {
+	j = 0;
+	for (int i = 0, n = strlen(old); i <= n; i++) {
+		if (old[i] == '&' || old[i] == '|' || old[i] == '<') {
 			new[j] = ' ';
-			new[j+1] = cmd[i];
+			new[j+1] = old[i];
 			new[j+2] = ' ';
 			j += 3;
-		} else if (cmd[i] == '>') {
-			if (cmd[i+1] != '>') {
+		} else if (old[i] == '>') {
+			if (old[i+1] == '>') {
 				new[j] = ' ';
-				new[j+1] = cmd[i];
-				new[j+2] = ' ';
-				j += 3;
-			} else {
-				new[j] = ' ';
-				new[j+1] = cmd[i];
-				new[j+2] = cmd[i+1];
+				new[j+1] = old[i];
+				new[j+2] = old[i+1];
 				new[j+3] = ' ';
 				j += 4;
-				i += 1;
+				i++;
+			} else {
+				new[j] = ' ';
+				new[j+1] = old[i];
+				new[j+2] = ' ';
+				j += 3;
 			}
 		} else {
-			new[j] = cmd[i];
+			new[j] = old[i];
 			j++;
 		}
 	}
-	new[strlen(cmd) + 1] = '\0';
-	return new;
+	new[j] = '\0';
 }
 
-void tokenizecmd(char cmd[]) {
+void tokenizecmd(char cmd[], char *tokens[]) {
+	int i;
 	char *token;
 
-	i = 0;
-
-	if (cmd != NULL) {
-		args[0] = strtok(cmd, " \t\n");
-		i = 1;
+	tokens[0] = strtok(cmd, " \n");
+	i = 1;
+	while ((token = strtok(NULL, " \n")) != NULL) {
+		tokens[i] = token;
+		i++;
 	}
-
-	while ((token = strtok(NULL, " \t\n")) != NULL) {
-		// Replace stdin
-		if (strcmp(token, "<") == 0) {
-			inredir(args, token);
-		// Truncate stdout
-		} else if (strcmp(token, ">") == 0) {
-			outredir(args, token, true);
-		// Append stdout
-		} else if (strcmp(token, ">>") == 0) {
-			outredir(args, token, false);
-		// Pipe
-		} else if (strcmp(token, "|") == 0) {
-			piperedir(args);
-		// Run in background
-		} else if (strcmp(token, "&") == 0) {
-			ampcmd(args);
-		} else {
-			args[i] = token;
-			i++;
-		}
-	}
+	tokens[i] = NULL;
 }
 
-void runcmd(char *args[]) {
+void runcmd(char *cmd[]) {
 	struct passwd *pw;
 	char *pwdir;
+	char execPath[EXECSZ];
+	int pid, wstatus, i, newcmdindex;
+	int inredir, outtrunc, outappend, amp, fd;
+	char *newcmd[ARGSZ];
 
-	if (strcmp(args[0], "cd") == 0) {
-		if (args[1] == NULL) {
+	// cd built-in
+	if (strcmp(cmd[0], "cd") == 0) {
+		if (cmd[1] == NULL) {
 			pw = getpwuid(getuid());
 			pwdir = pw->pw_dir;
 			chdir(pwdir);
 		} else {
-			chdir(args[1]);
+			chdir(cmd[1]);
 		}
+	// Run command if not built-in
 	} else {
+		inredir = 0;
+		outtrunc = 0;
+		outappend = 0;
+		amp = 0;
+
+		i = 0;
+		newcmdindex = 0;
+		while (cmd[i] != NULL) {
+			// Input redirection
+			if (strcmp(cmd[i],"<") == 0) {
+				inredir = i + 1;
+				i++;
+			// Output truncate
+			} else if (strcmp(cmd[i],">") == 0) {
+				outtrunc = i + 1;
+				i++;
+			// Output append
+			} else if (strcmp(cmd[i],">>") == 0) {
+				outappend = i + 1;
+				i++;
+			// Background
+			} else if (strcmp(cmd[i],"&") == 0) {
+				amp++;
+			} else {
+				newcmd[newcmdindex] = cmd[i];
+				newcmdindex++;
+			}
+			i++;
+		}
+		newcmd[newcmdindex] = NULL;
+
 		pid = fork();
 		if (pid == 0) {
+			if (inredir) {
+				fd = open(cmd[inredir], O_RDONLY);
+				dup2(fd, 0);
+			}
+			if (outtrunc) {
+				fd = open(cmd[outtrunc], O_WRONLY|O_TRUNC);
+				dup2(fd, 1);
+			}
+			if (outappend) {
+				fd = open(cmd[outappend], O_WRONLY|O_APPEND);
+				dup2(fd, 1);
+			}
+			sprintf(execPath, "/bin/%s", newcmd[0]);
+			execv(execPath, newcmd);
+			exit(0);
+		} else if (!amp) {
+			waitpid(pid, &wstatus, 0);
+		} else if (amp) {
+			printf("Process started in the background: %d\n", pid);
+		}
+	}
+}
+
+void piperedir(char *cmd[], int pipecount) {
+	int pfd[pipecount*2];
+	int pid, cmdindex, cmdno, wstatus;
+	char *args[ARGSZ];
+	char execPath[EXECSZ];
+
+	// Create pipes
+	for (int i = 0; i <= pipecount; i+=2) {
+		pipe(&pfd[i]);
+	}
+
+	// Run commands
+	cmdindex = 0;
+	for (cmdno = 0; cmdno <= pipecount; cmdno++) {
+		cmdindex = pipetokenize(cmd, cmdindex, args);
+		if (fork() == 0) {
+			// First command
+			if (cmdno == 0) {
+				dup2(pfd[1], 1);
+			// Last command
+			} else if (cmdno == (pipecount)) {
+				dup2(pfd[(pipecount*2)-2], 0);
+			// Middle commands, if any
+			} else {
+				dup2(pfd[(cmdno-1)*2], 0);
+				dup2(pfd[(cmdno*2)+1], 1);
+			}
+
+			// Close pipe fds and run command
+			for (int i = 0; i <=pipecount*2; i++) {
+				close(pfd[i]);
+			}
 			sprintf(execPath, "/bin/%s", args[0]);
 			execv(execPath, args);
 			exit(0);
-		} else {
-			waitpid(pid, &wstatus, 0);
 		}
+		cmdindex++;
+	}
+
+	for (int i = 0; i <= pipecount*2; i++) {
+		close(pfd[i]);
+	}
+	for (int i = 0; i <= pipecount; i++) {
+		wait(&wstatus);
 	}
 }
 
-void outredir(char *args[], char *token, bool trunc) {
+int pipetokenize(char *cmd[], int cmdindex, char *new[]) {
+	int newindex = 0;
 
-	args[i] = NULL;
-	token = strtok(NULL, " \t\n");
-
-	pid = fork();
-	if (pid == 0) {
-		if (trunc) {
-			fd = open(token, O_WRONLY | O_TRUNC);
-		} else {
-			fd = open(token, O_WRONLY | O_APPEND);
-		}
-		dup2(fd, 1);
-		sprintf(execPath, "/bin/%s", args[0]);
-		execv(execPath, args);
-		exit(0);
-	} else {
-		waitpid(pid, &wstatus, 0);
-		i = 0;
+	while (cmd[cmdindex] != NULL && strcmp(cmd[cmdindex], "|") != 0) {
+		new[newindex] = cmd[cmdindex];
+		newindex++;
+		cmdindex++;
 	}
-}
-
-void inredir(char *args[], char *token) {
-
-	args[i] = NULL;
-	token = strtok(NULL, " \t\n");
-
-	pid = fork();
-	if (pid == 0) {
-		fd = open(token, O_RDONLY);
-		dup2(fd, 0);
-		sprintf(execPath, "/bin/%s", args[0]);
-		execv(execPath, args);
-		exit(0);
-	} else {
-		waitpid(pid, &wstatus, 0);
-		i = 0;
-	}
-}
-
-void piperedir(char *args[]) {
-	char *token;
-	int pfd[2];
-
-	args[i] = NULL;
-
-	pipe(pfd);
-
-	// Write to pipe
-	if (fork() == 0) {
-		close(pfd[0]);
-		dup2(pfd[1], 1);
-		sprintf(execPath, "/bin/%s", args[0]);
-		execv(execPath, args);
-		exit(0);
-	}
-
-	// Get next command after pipe
-	i = 0;
-	while ((token = strtok(NULL, " \t\n")) != NULL) {
-		args[i] = token;
-		i++;
-	}
-	args[i] = NULL;
-
-	// Read from pipe
-	if (fork() == 0) {
-		close(pfd[1]);
-		dup2(pfd[0], 0);
-		sprintf(execPath, "/bin/%s", args[0]);
-		execv(execPath, args);
-		exit(0);
-	}
-
-	close(pfd[0]);
-	close(pfd[1]);
-	wait(&wstatus);
-	wait(&wstatus);
-	i = 0;
-}
-
-void ampcmd(char *args[]) {
-
-	args[i] = NULL;
-
-	pid = fork();
-	if (pid == 0) {
-		// to-do
-		sprintf(execPath, "/bin/%s", args[0]);
-		execv(execPath, args);
-		exit(0);
-	} else {
-		printf("Process in-progress: %d\n", pid);
-	}
-
-	i = 0;
+	new[newindex] = NULL;
+	return cmdindex;
 }
