d61b65e3449a347c69dab69de230c4a1874a4ee9
Owen Hamill <owen.oxley.hamill@gmail.com>
2021-01-04 10:42:48 -0500

    Updated echo.c

--- builtins/echo.c
+++ builtins/echo.c
@@ -3,18 +3,13 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    int i;
-
-    if (argc == 1) {
-        printf("\n");
-    } else {
-        for (i = 1; i < argc; i++) {
-            if (i != (argc - 1)) {
-                printf("%s ", argv[i]);
-            } else {
-                printf("%s", argv[i]);
-            }
+    
+    for (int i = 1; i < argc; i++) {
+        if (i != (argc - 1)) {
+            printf("%s ", argv[i]);
+        } else {
+            printf("%s", argv[i]);
         }
-        printf("\n");
     }
+    printf("\n");
 }
