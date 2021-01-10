1861499137fc54f2e44a4a905ac919d34f336ae9
Owen Hamill <owen.oxley.hamill@gmail.com>
2021-01-10 15:09:51 -0500

    Shell function declarations

---
+++ sh.h
@@ -0,0 +1,5 @@
+void addSpaces(char *, char[]);
+void tokenizecmd(char [], char*[], int*, int*);
+void runcmd(char *[], int);
+void runsemicoloncmd(char *[], int);
+int pipetokenize(char *[], int, char *[]);
